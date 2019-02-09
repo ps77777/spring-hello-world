fedb475640b3967edbb8fba3496981590f5a958c
stasik <stasik@wszib.edu.pl>
2019-02-09 12:56:30 +0100

    commit MyConfig2

---
+++ src/main/java/pl/edu/wszib/springhelloworld/model/MyConfiguration.java
@@ -0,0 +1,27 @@
+package pl.edu.wszib.springhelloworld.model;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class MyConfiguration {
+
+//    @Bean
+//    public Kot kot(){
+//        return new Kot();
+//    }
+//
+//    @Bean
+//    @Primary
+//    public Kot kot2(){
+//        return new Kot();
+//    }
+//
+//    @Bean
+//    public String execute ( Kot kot){
+//        return "";
+//    }
+
+}

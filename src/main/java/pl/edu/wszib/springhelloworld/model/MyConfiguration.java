86bdf3fcc43510f9f49c6c6a5106fbf1d3f86806
stasik <stasik@wszib.edu.pl>
2019-02-09 11:07:07 +0100

    commit MyConfig

---
+++ src/main/java/pl/edu/wszib/springhelloworld/model/MyConfiguration.java
@@ -0,0 +1,12 @@
+package pl.edu.wszib.springhelloworld.model;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+
+
+    }
+
